e3652748558cccd72048e464c1f079834890e614
Luis Mario Urrea Murillo <sinourain@gmail.com>
2014-08-25 12:40:07 -0500

    add attributes to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,28 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  	protect_from_forgery with: :exception
+  	before_action :authenticate_user!
+
+  	before_filter :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+	  # devise_parameter_sanitizer.for(:sign_up) << :full_name
+	  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
+	  	:email, 
+	  	:password, 
+	  	:password_confirmation, 
+	  	:current_password, 
+	  	:name, 
+	  	:last_name,
+	  	:description,
+	  	:phone,
+	  	:bird_date,
+	  	:gender,
+	  	:facebook,
+	  	:twitter,
+	  	:skype) }
+	end
 end
